b642967aee29823054d6f44f7f56efb0985210ab
naktu <dk@tutunak.com>
2016-06-13 15:01:12 +0300

    add todo

--- gif_grabber.py
+++ gif_grabber.py
@@ -6,7 +6,7 @@ from lxml import html
 
 FILEFORMAT = ".gif"
 
-
+links = []
 rss_xml = urllib.request.urlopen('http://ettoh.tumblr.com/rss').read()
 root = ET.fromstring(rss_xml)
 for child in root[0]:
@@ -15,4 +15,14 @@ for child in root[0]:
         images = tree.xpath("//img/@src")
         for i in images:
             if i[-4:] == FILEFORMAT:
-                print(i)
+                links.append(i)
+print(links)
+
+#TODO Get gif liks - function return object GIF
+#TODO Gif object:
+    # Have attr: link, path, 
+    # Have methods: get, hash
+    # get get file in needed path
+    # hash download file if path empty and create md5 hash
+    # if path defined - create md5 hash
+#TODO! Add tests for function and objects       
