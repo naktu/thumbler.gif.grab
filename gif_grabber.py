c321ad3de3220febc07cc44706c812c4eef0439f
naktu <dk@tutunak.com>
2016-06-13 14:14:41 +0300

    get only link to gif files

--- gif_grabber.py
+++ gif_grabber.py
@@ -4,14 +4,15 @@ import urllib.request
 import xml.etree.ElementTree as ET
 from lxml import html
 
+FILEFORMAT = ".gif"
+
 
 rss_xml = urllib.request.urlopen('http://ettoh.tumblr.com/rss').read()
 root = ET.fromstring(rss_xml)
-g = 0
 for child in root[0]:
-    g = g+1
     if child.tag == 'item':
         tree = html.fromstring(child.find('description').text)
         images = tree.xpath("//img/@src")
         for i in images:
-            print(i[-3:])
+            if i[-4:] == FILEFORMAT:
+                print(i)
