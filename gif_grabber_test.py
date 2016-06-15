2cfeb19b36d50ad9bb9a5c76fd1199cb408db09c
naktu <dk@tutunak.com>
2016-06-15 13:35:43 +0300

    add unittest structure to project

---
+++ gif_grabber_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import unittest
+import gif_grabber
+
+class TestGetRssXml(unittest.TestCase):
+    def setUP(self):
+        valid_url    = "http://animesquads.tumblr.com/rss"
+        no_exist_url = "http://animesquads.dtumblr.com/rss"
+
+    def test_valid_rss_url:
+        #TODO check valid url
+        pass
+    
+    def test_url_unvaliable:
+        #TODO check with no_exist_url
+        pass
+
+    def test_no_rss_url:
+        #TODO check if url no rss
+        pass
